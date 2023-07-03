49f81e1c4b469c0566dcc9025a0a88f3f3c7294d
anishabid-fandist <100930746+anishabid-fandist@users.noreply.github.com>
2023-07-03 12:25:57 +0500

    return added

--- controllers/reminders.js
+++ controllers/reminders.js
@@ -2,52 +2,81 @@ const Reminders = require('../models/reminders');
 const {Op} = require('sequelize');
 
 const setReminder = async (req,res)=>{
-
+try{
     let result = await Reminders.create(req.body)
-    res.status(201).json(result)
+    return  res.status(201).json(result)
+}catch(e){
+    return res.status(500).json(e)
+}
+    
 }
 const getReminder = async (req,res)=>{
-    let query = {}
-    if(!!req.query)
-        query={where:{}}
-    if(req.query.user)
-        query.where={...query.where,user:req.query.user}
-    if(req.query.after)
-    query.where={...query.where,date:{[Op.gte]:new Date(parseInt(req.query.after))}}    
-
-    let result = await Reminders.findAll(query)
-    res.status(200).json(result)
+    try{
+        let query = {}
+        if(!!req.query)
+            query={where:{}}
+        if(req.query.user)
+            query.where={...query.where,user:req.query.user}
+        if(req.query.after)
+        query.where={...query.where,date:{[Op.gte]:new Date(parseInt(req.query.after))}}    
+    
+        let result = await Reminders.findAll(query)
+        return res.status(200).json(result)
+    }catch(e){
+        return res.status(500).json(e)
+    }
+    
 }
 const getReminderById = async (req,res)=>{
-    let result = await Reminders.findOne({where: {
-        id:req.params.id
-    }})
-    if(!result)
-        res.status(404)
-    res.status(200).json(result)
+    try{
+        let result = await Reminders.findOne({where: {
+            id:req.params.id
+        }})
+        if(!result)
+            res.status(404)
+            return res.status(200).json(result)
+    }catch(e){
+        return res.status(500).json(e)
+    
+}
 }
 const deleteReminder = async (req,res)=>{
-    let result = await Reminders.destroy({
-        where: {
-            id:req.params.id
-        }
-      })
-      if(!result)
-        res.status(404)
-    res.status(200).json(result)
+    try{
+        let result = await Reminders.destroy({
+            where: {
+                id:req.params.id
+            }
+          })
+          if(!result)
+            res.status(404)
+        res.status(200).json(result)
+    }catch(e){
+        res.status(500).json(e)
+    }
+    
 }
 const updatePatch = async (req,res)=>{
-    let result = await Reminders.update(req.body,{where:{id:req.params.id}})
-    if(!result)
-        res.status(404)
-
-    res.status(200).json(result)
+    try{
+        let result = await Reminders.update(req.body,{where:{id:req.params.id}})
+        if(!result)
+            res.status(404)
+    
+        res.status(200).json(result)
+    }catch(e){
+        res.status(500).json(e)
+    }
+    
 }
 const update = async (req,res)=>{
-    let result = await Reminders.update(req.body,{where:{id:req.params.id}})
-    if(!result)
-        res.status(404)
-    res.status(200).json(result)
+    try{
+        let result = await Reminders.update(req.body,{where:{id:req.params.id}})
+        if(!result)
+            res.status(404)
+        res.status(200).json(result)
+    }catch(e){
+        res.status(500).json(e)
+    }
+    
 }
 module.exports = {
     setReminder,
